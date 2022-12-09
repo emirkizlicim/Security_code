df8d236206e29ded777c8a03ad1dbc2c1755f26f
Emir Kızılçim <emirkzlcm0@gmail.com>
2022-12-09 18:58:57 +0300

    Security code generation
    Generating new code every single time as 4 digits.

---
+++ main.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+
+    while (true)
+    {
+
+        srand((unsigned)time(NULL));
+
+        std::cout << "Your code has been generating." << std::endl;
+
+        int code = (1000 + rand() % 9001);
+
+
+        std::cout << code << std::endl;
+        std::cout << "please enter the code correctly." << std::endl;
+
+        int code_input;
+        std::cin >> code_input;
+
+        if (code_input == code)
+        {
+            std::cout << "You have successfully entered the code." << std::endl;
+            break;
+        }
+        else {
+            std::cout << "You have entered the false code." << std::endl;
+            std::cout << "Please try again." << std::endl;
+            std::cout << "Please wait until the new code will be generated." << std::endl;
+            Sleep(9999);
+            continue;
+        }
+
+    }
+    return 0;
+}
